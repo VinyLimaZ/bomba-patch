0b3ba713492de4408c623c89a1be959f672221ba
Lucas Santos da Costa <lsantosc@live.com>
2022-09-27 21:32:55 -0300

    Generate default users from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+unless User.any?
+  puts 'Criando usuários admin'
+  default_password = 'foobar123'
+
+  users = [
+    { name: 'Lucas Santos da Costa', email: 'lucas.costa@clicksign.com', password: default_password },
+    { name: 'Vinicius Lima', email: 'vinicius.lima@clicksign.com', password: default_password },
+    { name: 'Deyvid Nascimento', email: 'deyvid.nascimento@clicksign.com', password: default_password }
+  ]
+
+  users.each do |user|
+    if User.create(user)
+      puts "Criado usuário #{user[:name]} - #{user[:email]}"
+    end
+  end
+end
