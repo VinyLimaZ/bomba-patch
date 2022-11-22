d3c3a37b2719c6e9a0114e6727aeb46a3acd44e3
Dérick Hogan Pimenta <derickhoganpimenta@yahoo.com.br>
2022-11-21 23:12:36 -0300

    add cable events

--- app/javascript/controllers/matches_controller.js
+++ app/javascript/controllers/matches_controller.js
@@ -5,4 +5,20 @@ export default class extends Controller {
 
   static targets = [  ]
 
+  connect() {
+    console.log("aqui")
+  }
+
+  _cableConnected() {
+    // Called when the subscription is ready for use on the server
+  }
+
+  _cableDisconnected() {
+    // Called when the subscription has been terminated by the server
+  }
+
+  _cableReceived(data) {
+    console.log(data)
+  }
+
 }
